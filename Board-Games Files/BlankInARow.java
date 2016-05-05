29650e51c3224170271ad31d934a8e14b9a92edc
Alex Bian <abian8@students.d125.org>
2016-05-05 14:22:11 -0500

    blankinarow interface

---
+++ Board-Games Files/BlankInARow.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of interface BlankInARow here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface BlankInARow
+{
+    /**
+     * An example of a method header - replace this comment with your own
+     * 
+     * @param  y    a sample parameter for a method
+     * @return        the result produced by sampleMethod 
+     */
+    int numInARow(int num);
+    boolean victoryOrNaw(boolean[][] board);
+    
+}
