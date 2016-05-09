b915443ab3c4a146a1a9404dc8e501312aad9b90
Alex Bian <abian8@students.d125.org>
2016-05-09 14:15:21 -0500

    ticTacToeRunner

--- Board-Games Files/TicTacToeRunner.java
+++ Board-Games Files/TicTacToeRunner.java
@@ -10,11 +10,28 @@ public class TicTacToeRunner
     // instance variables - replace the example below with your own
     public static void main(String[] args)
     {
+        String str;
+        int row=0, col=0, indexOfComma;
+        System.out.print("Enter the coordinates of the square in the format(#,#), seperate by a comma with no space: ");
         Scanner sc = new Scanner(System.in);
-        
         TicTacToeBoard hi = new TicTacToeBoard();
         
-        String str = sc.nextLine();
+        while(!hi.gameOver())
+        {
+            System.out.print("Player " + hi.getPlayer() + ", please make a move: ");
+            str = sc.nextLine();
+            indexOfComma=str.indexOf(",");
+            while(indexOfComma==-1 || (Integer.parseInt(str.substring(0,indexOfComma))>2 || Integer.parseInt(str.substring(0,indexOfComma))<0)
+            || (Integer.parseInt(str.substring(indexOfComma))>2 || Integer.parseInt(str.substring(indexOfComma))<0))
+            {
+                System.out.print("Invalid move. Player " + hi.getPlayer() + ", please make a legal move: ");
+                str=sc.nextLine();
+            }
+            
+            row=Integer.parseInt(str.substring(0,indexOfComma));
+            col=Integer.parseInt(str.substring(indexOfComma));
+            
+        }
         
     }
 }
